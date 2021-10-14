4fd3913714eda9ce45ae7eb0b91dcab5c5a7904e
Daniel Gomez-Sanchez <daniel.gomez-sanchez@allianz.at>
2021-10-14 17:08:38 +0200

    Move 'PreferencesImpl' to a different package

--- bundles/org.aposin.gem.core/src/org/aposin/gem/core/impl/internal/config/prefs/PreferencesImpl.java
+++ bundles/org.aposin.gem.core/src/org/aposin/gem/core/impl/internal/config/prefs/PreferencesImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.aposin.gem.core.impl.internal;
+package org.aposin.gem.core.impl.internal.config.prefs;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
